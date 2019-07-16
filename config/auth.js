7a6c411b26629087b51fd0ec6576374b56f0bac9
Yogi Triono <ogietriyono@gmail.com>
2019-07-16 22:25:31 +0700

    tambah autentikasi token

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports.verifyToken = (req, res, next) => {
     if (typeof bearerHeader !== 'undefined') {
         const bearer = bearerHeader.split(' ')
         const bearerToken = bearer[1]
-        req.token = bearerToken
+        req.token = bearerToken;
         next()
     } else {
         res.sendStatus(403);

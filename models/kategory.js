edac77593836ac03c5cb48b5dc106e199bffee75
DiniAnggraini026 <dinianggraini026@gmail.com>
2019-07-19 15:11:53 +0700

    Memperbaiki field category

--- models/kategory.js
+++ models/kategory.js
@@ -4,7 +4,7 @@ const sequelize = require('../config/db_sequelize.js')
 class Kategory extends Sequelize.Model {}
 
 Kategory.init({
-    name_category: Sequelize.STRING,
+    category_name: Sequelize.STRING,
 }, {
     sequelize,
     modelName: 'category'

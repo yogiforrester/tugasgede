4a7564d83e08e3fdcbf757168b08a414feb03d71
DiniAnggraini026 <dinianggraini026@gmail.com>
2019-07-19 15:34:31 +0700

    Menambahkan fungsi pada bookController.js

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -2,9 +2,11 @@ const books = require('../models/books.js')
 const kategory = require('../models/Kategory.js')
 const Katalogku = require('../models/Katalogku.js')
 const fs = require('fs')
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 
-books.belongsTo(kategory)
-books.belongsTo(katalogku)
+dotenv.config();
+books.belongsTo(Kategory)
 
 module.exports.getAllBooks = (req, res) => {
     books.findAll().then((books) => {
@@ -12,19 +14,87 @@ module.exports.getAllBooks = (req, res) => {
     })
 }
 
+module.exports.getBooksId = (req, res) => {
+	books.findByPk(req.params.id).then(buku => {
+		res.json(buku)
+	})
+}
+
 module.exports.createBooks = (req, res) => {
+    jwt.verify(req.token, process.env.SECRETKEY, (error,authData)=>{
+		if (error) {
+			res.sendStatus(403);
+		}else{
+			if (authData['roles']=="admin") {
     books.create({
         judul_buku: req.body.judul_buku,
         pengarang: req.body.pengarang,
         penerbit: req.body.penerbit,
         jumlah_hal: req.body.jumlah_hal,
-        gambar: req.body.gambar,
         harga: req.body.harga,
         deskripsi_buku: req.body.deskripsi_buku,
         tanggal_rilis: req.body.tanggal_rilis,
-        categoryId: req.body.id_kategori,
-        catalogId: req.body.id_katalog
+        category_name: req.body.kategori_name
     }).then((books) => {
         res.json(books)
     })
+
+        }else{
+            res.json("anda Harus Login Sebagai Admin Untuk Bisa menambah Buku")
+         }
+    }
 }
+)}
+
+module.exports.updatebooks = (req, res) => {
+    jwt.verify(req.token, process.env.SECRETKEY, (error,authData)=>{
+        if (error) {
+            res.sendStatus(403);
+    }else{
+        if (authData['roles']=="admin") {
+        books.update({
+            judul_buku: req.body.judul_buku,
+            pengarang: req.body.pengarang,
+            penerbit: req.body.penerbit,
+            jumlah_hal: req.body.jumlah_hal,
+            harga: req.body.harga,
+            deskripsi_buku: req.body.deskripsi_buku,
+            tanggal_rilis: req.body.tanggal_rilis,
+            category_name: req.body.category_name,
+            categoryId: req.body.id_kategori
+         }, {
+            where: {
+                id: req.params.id
+             }
+        }).then((books) => {
+              res.json(books);
+        }).catch((error) => {
+             throw error;
+        })
+    }
+    }
+    }
+    )
+}
+
+
+module.exports.deletebooks = (req, res) => {
+    jwt.verify(req.token, process.env.SECRETKEY, (error,authData)=>{
+        if (error) {
+            res.sendStatus(403);
+        }else{
+            if (authData['roles']=="admin") {
+        books.destroy({
+            where: {
+                id: req.params.id
+        }   
+    }).then((books) => {
+        res.json(books);
+    }).catch((error) => {
+        throw error;
+    })
+        }
+        }
+    })
+}
+

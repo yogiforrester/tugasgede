c8786d25dd40edab1d26b8f4eecde815230ee498
DiniAnggraini026 <dinianggraini026@gmail.com>
2019-07-19 15:07:26 +0700

    Penambahan file model user.js

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../config/db_sequelize');
+
+class User extends Sequelize.Model {}
+
+User.init ({
+	username: Sequelize.STRING,
+	email: Sequelize.STRING,
+	password: Sequelize.STRING,
+	roles: Sequelize.STRING,
+}, { sequelize, modelName: 'user' });
+
+module.exports = User;
